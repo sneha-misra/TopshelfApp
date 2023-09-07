42759b73df5750f1f0b458ac98f77b9f5f7fb6d5
sneha-misra <142022086+sneha-misra@users.noreply.github.com>
2023-09-07 18:50:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//// See https://aka.ms/new-console-template for more information
+using Topshelf;
+using static Topshelf.ConsoleApp.Service;
+
+//Console.WriteLine("Hello, World!");
+
+class Program {
+    static void Main(string[] args)
+    {
+        HostFactory.Run(x =>
+        {
+            x.Service<LoggingService>();
+            x.EnableServiceRecovery(r => r.RestartService(TimeSpan.FromSeconds(10)));
+            x.SetServiceName("TestService");
+            x.StartAutomatically();
+        }
+        );
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topshelf.ConsoleApp
+{
+    internal class Service
+    {
+        public class LoggingService : ServiceControl
+        {
+            private const string _logFileLocation = @"D:\Projects\.netCore\servicelog.txt";
+
+            private void Log(string logMessage)
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(_logFileLocation));
+                File.AppendAllText(_logFileLocation, DateTime.UtcNow.ToString() + " : " + logMessage + Environment.NewLine);
+            }
+
+            public bool Start(HostControl hostControl)
+            {
+                Log("Starting");
+                return true;
+            }
+
+            public bool Stop(HostControl hostControl)
+            {
+                Log("Stopping");
+                return true;
+            }
+        }
+    }
+}
